44aef9eab34ceadb21cefc633e6a9f74c55fece8
Artem <154594695+Nighty3098@users.noreply.github.com>
2026-02-23 17:13:12 +0700

    Update

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -21,9 +21,9 @@ const projects_data = {
     {
       id: 2,
       title: "GhostlyGrabber",
-      info: "A utility for automatically downloading media files from Telegram",
+      info: "A OSINT tool for automated collection and analysis of data from Telegram channels and chats with complete content archiving",
       description:
-        "A utility for automatically downloading media files from Telegram channels and chats with convenient storage and organization of content",
+        "A OSINT tool for automated collection and analysis of data from Telegram channels and chats with complete content archiving",
       image: "/images/grabber.png",
       link: "https://github.com/He4vyL0v3/GhostlyGrabber/",
     },
