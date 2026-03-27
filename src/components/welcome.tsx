7d993ca4bc515e58f0890f0a3bd7c7ec58e3e089
Artem <154594695+Nighty3098@users.noreply.github.com>
2026-03-27 12:20:02 +0700

    Fix

--- src/components/welcome.tsx
+++ src/components/welcome.tsx
@@ -4,10 +4,7 @@ function Welcome() {
   return (
     <div className="welcome">
       <h1>He4vyL0v3</h1>
-
-      <div className="float-text">
-        <AnimatedBlurText text="Big brother is watching you" duration={1300} />
-      </div>
+      <AnimatedBlurText text="Big brother is watching you" duration={1300} />
     </div>
   );
 }
